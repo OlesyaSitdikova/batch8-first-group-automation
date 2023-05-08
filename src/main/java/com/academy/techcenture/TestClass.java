5b4b418dc1db7da1a1eb8154322bd9108251db40
OlesyaSitdikova <olesyabuyselldc@gmail.com>
2023-05-07 21:06:01 -0400

    added TestClass

---
+++ src/main/java/com/academy/techcenture/TestClass.java
@@ -0,0 +1,7 @@
+package com.academy.techcenture;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("This is first added test class by olesya ");
+    }
+}
